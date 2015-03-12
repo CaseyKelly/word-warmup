f7c42c6eed49626bbc07c37d32beddbfae1dcf96
CaseyKelly <casey.kelly@colorado.edu>
2015-03-12 09:20:22 -0600

    word counter works

--- lib/word_cloud.rb
+++ lib/word_cloud.rb
@@ -1,3 +1,12 @@
 class WordCloud
 
+   def initialize(sayings)
+     sayings=sayings.join.downcase.to_s;
+     hash = Hash.new(0)
+     sayings.split.each do |word|
+       hash[word] += 1
+      end
+      return hash.to_a;
+   end
+
 end
