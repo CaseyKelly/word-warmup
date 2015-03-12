ddc5464fe65bb3757be98344767377a1781f9523
CaseyKelly <casey.kelly@colorado.edu>
2015-03-12 09:24:23 -0600

    refactor the answer

--- lib/word_cloud.rb
+++ lib/word_cloud.rb
@@ -3,10 +3,8 @@ class WordCloud
    def initialize(sayings)
      sayings=sayings.join.downcase.to_s;
      hash = Hash.new(0)
-     sayings.split.each do |word|
-       hash[word] += 1
-      end
-      return hash.to_a;
+     sayings.split.each {|word| hash[word] += 1}
+      return hash
    end
 
 end
